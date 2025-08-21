4a3b0391fc2ac1db987f2f61ce0a8b26c4368d8a
María Zapata <54915677+karou1182001@users.noreply.github.com>
2025-08-21 15:46:45 -0400

    lght and dark theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 import { useState, useEffect } from "react";
+import {cn} from "@/lib/utils";
 
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -32,8 +33,11 @@ export const ThemeToggle = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 rounded-md transition-colors hover:bg-muted"
-      aria-label="Toggle Theme"
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden"
+        )}
+        
     >
       {isDarkMode ? (
         <Sun className="h-6 w-6 text-yellow-300" />
