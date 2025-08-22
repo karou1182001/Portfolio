a383bd9ad43c62ef8aad992e5e37fe5a331ec4af
María Zapata <54915677+karou1182001@users.noreply.github.com>
2025-08-21 21:56:31 -0400

    Projects section

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,3 +1,5 @@
+import { ExternalLink, Github, ArrowRight} from "lucide-react";
+
 const projects = [
     {
         id:1,
@@ -38,6 +40,45 @@ export const ProjectSection = () => {
             <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
                 Here are some of my recent projects. Each project special
             </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {projects.map((project, key) => (
+                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover ">
+                        <div className="h-48 overflow-hidden">
+                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                        </div>
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag)=>(
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                        
+                        
+                            <h3 className="text-xl font-semibold mb-1">
+                                {project.title}
+                            </h3>
+
+                            <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a href={project.demoURL} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300"><ExternalLink size={20}/></a>
+                                    <a href={project.githubURL} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300"><Github size={20}/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="text-center mt-12 ">
+                <a className="cosmic-button w-fit flex items-center mx-auto gap-2" target="_blank" href="https://github.com/karou1182001">
+                    Check My Github <ArrowRight size={16}/>
+                </a>
+            </div>
         </div>
     </section>
 };
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -64,7 +64,7 @@ export const SkillSection = () => {
                             <h3 className="font-semibold text-lg">{skill.name}</h3>
                         </div>
                         <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
-                            <div className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out" style={{width: skill.level+"%"}}/>
+                            <div className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]" style={{width: skill.level+"%"}}/>
                         </div>
                         <div className="text-right mt-1">
                             <span className="text-sm text-muted-foreground">{skill.level}%</span>
