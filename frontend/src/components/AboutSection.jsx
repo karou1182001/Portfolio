e58282dee4d8dc7f8d37d077b257d0fb2983a636
María Zapata <54915677+karou1182001@users.noreply.github.com>
2025-08-29 21:21:11 -0400

    resume

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -64,7 +64,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1qv52-4alrlE5rWVqfK2pEp_HV2PLEcCg/view?usp=sharing"
+                href="https://drive.google.com/file/d/1fels8-CGZu24GBybyqlD66cvF6diNC27/view?usp=sharing"
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
